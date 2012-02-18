150fdda0c4bc1a3cbf273f4b20fe2e318f470dca
B. Clare Smith <clare@ianclaresmith.com>
2012-02-18 13:12:46 +0000

    after restart

--- README.markdown
+++
@@ -1,6 +0,0 @@
-# Ruby on Rails first application
-
-MonthByMonth
-
-Bendafé Month by Month application test
-
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MonthByMonth::Application.config.secret_token = 'eebbaa459d39177d2b164da36deaa9bc764a27e4ef241a1956ca229c36a98c2c252bb3f4f5eea770158af2dfd94ca3abbf56c910600f4fe415ee3e759431bbcf'
+MonthByMonth::Application.config.secret_token = '727a8a3266a00350cbf3d631328fbb3c6daf5e2ab9e7ee3ee335326e2a5a1001cb0a64021b6f352d6e5b8c8f15484229a8090f3ba60dba4b37e82b65741cb27f'
